9f4f5d88249f82f7a16e8fa6d75b6b5aea9f2487
brev4 <tisodev99@gmail.com>
2026-08-09 14:18:38 -0400

    BATH PATH - Workplace Finder

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: (process.env.BASE_PATH ?? '/workplacefinder/') as string,
+  base: (process.env.BASE_PATH ?? './workplacefinder') as string,
   plugins: [react(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
