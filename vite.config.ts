2fce39e42aad8f6f4c42b082c9a901634beb25d6
brev4 <tisodev99@gmail.com>
2026-08-09 14:20:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: (process.env.BASE_PATH ?? './workplacefinder') as string,
+  base: '/workplacefinder/',
   plugins: [react(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
